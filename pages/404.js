a7d5df34795529d5daf41fdd2800d9c675e2531c
Shalihin Ahmad <shalihinsgp@gmail.com>
2022-11-07 21:10:09 +0100

    404 page redirects to homepage

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-const NotFound = () => (
-  <div className="not-found">
-    <h1>Ooooopss.....</h1>
-    <h2>That page cannot be found.</h2>
-    <p>
-      Go back to the
-      {' '}
-      <Link href="/">Homepage</Link>
-    </p>
-  </div>
-);
+const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      // router.go(1); // can go back and forth through the history
+      router.push('/'); // redirect to the homepage after 3 seconds
+    }, 3000);
+  }, []);
+
+  return (
+    <div className="not-found">
+      <h1>Ooooopss.....</h1>
+      <h2>That page cannot be found.</h2>
+      <p>
+        Go back to the
+        {' '}
+        <Link href="/">Homepage</Link>
+      </p>
+    </div>
+  );
+};
 
 export default NotFound;
