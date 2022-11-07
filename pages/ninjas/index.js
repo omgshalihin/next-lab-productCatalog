c73e12c027f8378c00c9ef865e072dfbb3bf6098
Shalihin Ahmad <shalihinsgp@gmail.com>
2022-11-07 23:07:03 +0100

    dynamic id page done

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Link from 'next/link';
+import styles from '../../styles/Ninjas.module.css';
+
+// this runs before component is rendered
+export const getStaticProps = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await response.json(); // data is an array of objects
+  return {
+    props: { ninjas: data },
+  };
+};
 
 // http://localhost:3000/ninjas
-const Ninjas = () => (
+const Ninjas = ({ ninjas }) => (
   <div>
     <h1>All Ninjas</h1>
+    {ninjas.map(ninja => (
+      <Link href={`/ninjas/${ninja.id}`} className={styles.single} key={ninja.id}>
+        <h3>{ninja.name}</h3>
+      </Link>
+    ))}
   </div>
 );
 
